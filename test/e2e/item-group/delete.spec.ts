0f2e3cc80160593c985e97e8ed60acfe51d26143
Martien Dermawan Tanama <martiendt@gmail.com>
2023-02-27 11:46:21 +0700

    item-group

---
+++ test/e2e/item-group/delete.spec.ts
@@ -0,0 +1,72 @@
+import request from "supertest";
+import { createApp } from "@src/app.js";
+
+describe("delete item", () => {
+  let _id = "";
+  beforeEach(async () => {
+    const app = await createApp();
+    // get access token for authorization request
+    const authResponse = await request(app).patch("/v1/auth/signin").send({
+      username: "admin",
+      password: "admin2024",
+    });
+    const accessToken = authResponse.body.accessToken;
+    // send request to create item
+    const data = {
+      name: "item A",
+    };
+    const response = await request(app)
+      .post("/v1/item-groups")
+      .send(data)
+      .set("Authorization", `Bearer ${accessToken}`);
+    _id = response.body._id;
+  });
+  it("should check user is authorized", async () => {
+    const app = await createApp();
+    // send request to create item
+    const response = await request(app).delete("/v1/item-groups/" + _id);
+    expect(response.statusCode).toEqual(401);
+    expect(response.body.message).toBe("Unauthorized Access");
+  });
+  it("should check user have permission to access", async () => {
+    const app = await createApp();
+    // get access token for authorization request
+    const authResponse = await request(app).post("/v1/auth/signin").send({
+      username: "user",
+      password: "user2024",
+    });
+    const accessToken = authResponse.body.accessToken;
+    // send request to read item
+    const response = await request(app)
+      .delete("/v1/item-groups/" + _id)
+      .set("Authorization", `Bearer ${accessToken}`);
+
+    expect(response.statusCode).toEqual(403);
+    expect(response.body.message).toBe("Forbidden Access");
+  });
+  it("should delete data from database", async () => {
+    const app = await createApp();
+    // get access token for authorization request
+    const authResponse = await request(app).post("/v1/auth/signin").send({
+      username: "admin",
+      password: "admin2024",
+    });
+    const accessToken = authResponse.body.accessToken;
+    const responseDelete = await request(app)
+      .delete("/v1/item-groups/" + _id)
+      .set("Authorization", `Bearer ${accessToken}`);
+    // expected response status
+    expect(responseDelete.statusCode).toEqual(204);
+
+    const response = await request(app).get("/v1/item-groups").set("Authorization", `Bearer ${accessToken}`);
+    // expected response status
+    expect(response.statusCode).toEqual(200);
+    // expected response body
+    expect(response.body.data.length).toBe(0);
+
+    expect(response.body.pagination.page).toEqual(1);
+    expect(response.body.pagination.pageCount).toEqual(0);
+    expect(response.body.pagination.pageSize).toEqual(10);
+    expect(response.body.pagination.totalDocument).toEqual(0);
+  });
+});
---
+++ test/e2e/item-group/read.spec.ts
@@ -0,0 +1,133 @@
+import request from "supertest";
+import { createApp } from "@src/app.js";
+
+describe("list all items", () => {
+  it("should check user is authorized", async () => {
+    const app = await createApp();
+    // send request to create item
+    const response = await request(app).get("/v1/item-groups");
+    expect(response.statusCode).toEqual(401);
+    expect(response.body.message).toBe("Unauthorized Access");
+  });
+  it("should check user have permission to access", async () => {
+    const app = await createApp();
+    // get access token for authorization request
+    const authResponse = await request(app).post("/v1/auth/signin").send({
+      username: "user",
+      password: "user2024",
+    });
+    const accessToken = authResponse.body.accessToken;
+    // send request to read item
+    const response = await request(app).get("/v1/item-groups").set("Authorization", `Bearer ${accessToken}`);
+
+    expect(response.statusCode).toEqual(403);
+    expect(response.body.message).toBe("Forbidden Access");
+  });
+  it("should read data from database", async () => {
+    const app = await createApp();
+    // get access token for authorization request
+    const authResponse = await request(app).post("/v1/auth/signin").send({
+      username: "admin",
+      password: "admin2024",
+    });
+    const accessToken = authResponse.body.accessToken;
+
+    // create data
+    const data = {
+      name: "item A",
+    };
+    await request(app).post("/v1/item-groups").send(data).set("Authorization", `Bearer ${accessToken}`);
+    const data2 = {
+      name: "item B",
+    };
+    await request(app).post("/v1/item-groups").send(data2).set("Authorization", `Bearer ${accessToken}`);
+
+    const response = await request(app).get("/v1/item-groups").set("Authorization", `Bearer ${accessToken}`);
+    // expected response status
+    expect(response.statusCode).toEqual(200);
+    // expected response body
+    expect(response.body.data[0]._id).not.toBeNull();
+    expect(response.body.data[0].name).toEqual(data.name);
+    expect(response.body.data[0].createdAt instanceof Date).toBeTruthy();
+    expect(response.body.data[0].createdBy_id).toBe(authResponse.body._id);
+
+    expect(response.body.data[1]._id).not.toBeNull();
+    expect(response.body.data[1].name).toEqual(data2.name);
+    expect(response.body.data[1].createdAt instanceof Date).toBeTruthy();
+    expect(response.body.data[1].createdBy_id).toBe(authResponse.body._id);
+
+    expect(response.body.pagination.page).toEqual(1);
+    expect(response.body.pagination.pageCount).toEqual(1);
+    expect(response.body.pagination.pageSize).toEqual(10);
+    expect(response.body.pagination.totalDocument).toEqual(2);
+  });
+});
+
+describe("read item", () => {
+  it("should check user is authorized", async () => {
+    const app = await createApp();
+    // send request to create item
+    const response = await request(app).get("/v1/item-groups");
+    expect(response.statusCode).toEqual(401);
+    expect(response.body.message).toBe("Unauthorized Access");
+  });
+  it("should check user have permission to access", async () => {
+    const app = await createApp();
+    // get access token for authorization request
+    const authResponse = await request(app).post("/v1/auth/signin").send({
+      username: "user",
+      password: "user2024",
+    });
+    const accessToken = authResponse.body.accessToken;
+    // send request to read item
+    const response = await request(app).get("/v1/item-groups").set("Authorization", `Bearer ${accessToken}`);
+
+    expect(response.statusCode).toEqual(403);
+    expect(response.body.message).toBe("Forbidden Access");
+  });
+  it("should read data from database", async () => {
+    const app = await createApp();
+    // get access token for authorization request
+    const authResponse = await request(app).post("/v1/auth/signin").send({
+      username: "admin",
+      password: "admin2024",
+    });
+    const accessToken = authResponse.body.accessToken;
+
+    // create data
+    const data = {
+      code: "A1",
+      name: "item A",
+      chartOfAccount: "Goods",
+      hasProductionNumber: true,
+      hasExpiryDate: false,
+      unit: "pcs",
+      converter: [
+        {
+          name: "dozen",
+          multiply: 12,
+        },
+      ],
+    };
+    const responseCreate = await request(app)
+      .post("/v1/item-groups")
+      .send(data)
+      .set("Authorization", `Bearer ${accessToken}`);
+    const response = await request(app)
+      .get("/v1/item-groups/" + responseCreate.body._id)
+      .set("Authorization", `Bearer ${accessToken}`);
+    // expected response status
+    expect(response.statusCode).toEqual(200);
+    // expected response body
+    expect(response.body.data._id).not.toBeNull();
+    expect(response.body.data.code).toEqual(data.code);
+    expect(response.body.data.name).toEqual(data.name);
+    expect(response.body.data.chartOfAccount).toEqual(data.chartOfAccount);
+    expect(response.body.data.hasProductionNumber).toEqual(data.hasProductionNumber);
+    expect(response.body.data.hasExpiryDate).toEqual(data.hasExpiryDate);
+    expect(response.body.data.unit).toEqual(data.unit);
+    expect(response.body.data.converter).toEqual(data.converter);
+    expect(response.body.data.createdAt instanceof Date).toBeTruthy();
+    expect(response.body.data.createdBy_id).toBe(authResponse.body._id);
+  });
+});
